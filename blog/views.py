1f0e1ad54989f4c76758f25efdc95f12b4c01dc5
Iraqez <vladimir.gumenyuk@gmail.com>
2025-04-06 12:08:02 +0300

    add blog views and template

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,4 @@
-from django.shortcuts import render
-from django.http import Http404
+from django.shortcuts import get_object_or_404, render
 
 from .models import Post
 def post_list(request):
@@ -11,12 +10,13 @@ def post_list(request):
     )
 
 def post_detail(request, id):
-    try:
-        post = Post.published.get(id=id)
-    except Post.DoesNotExist:
-        raise Http404("Пост не знайдено")
+    post = get_object_or_404(
+        Post,
+        id=id,
+        status=Post.Status.PUBLISHED
+    )
     return render(
         request,
         'blog/post/detail.html',
-        {post: post}
+        {'post': post}
     )
