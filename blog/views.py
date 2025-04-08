efd3983a3fbbfae0d45645d25fd55c2c92b811bf
Iraqez <vladimir.gumenyuk@gmail.com>
2025-04-08 12:52:25 +0300

    classViews

--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,13 @@
 from django.shortcuts import get_object_or_404, render
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Post
 
-def post_list(request):
-    post_list = Post.published.all()
-    paginator = Paginator(post_list, 3)
-    page_number = request.GET.get('page', 1)
-    try:
-        posts = paginator.page(page_number)
-    except PageNotAnInteger:
-        posts = paginator.page(1)
-    except EmptyPage:
-        posts = paginator.page(paginator.num_pages)
+from django.views.generic import ListView
 
-    return render(
-        request,
-        'blog/post/list.html',
-        {'posts': posts}
-    )
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 10
+    template_name = 'blog/post/list.html'
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(
---
+++ create_posts.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.models import User
+from blog.models import Post
+import lorem
+
+user = User.objects.get(username = 'iraqez')
+
+def lorPost(x, y):
+    for i in range(x, y):
+        lor = lorem.get_paragraph(count=3)
+        Post(title='Тестовий пост №'+str(i), slug='post-from-shell'+str(i), body=lor, author=user, status='PB').save()
+
+if __name__ == '__main__':
+    lorPost(1, 10)
