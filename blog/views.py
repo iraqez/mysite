5f618886d4f55be57ffd08e6e7f050e4e20b4b2e
Iraqez <vladimir.gumenyuk@gmail.com>
2025-04-08 13:31:20 +0300

    add Post

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import get_object_or_404, render
 from .models import Post
+from .forms import EmailPostForm
 
 from django.views.generic import ListView
 
@@ -22,4 +23,25 @@ def post_detail(request, year, month, day, post):
         request,
         'blog/post/detail.html',
         {'post': post}
+    )
+
+def post_share(request, post_id):
+    post = get_object_or_404(
+        Post,
+        id=post_id,
+        status=Post.Status.PUBLISHED,
+    )
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+    else:
+        form = EmailPostForm()
+    return render(
+        request,
+        'blog/post/share.html',
+        {
+            'post': post,
+            'form': form
+        }
     )
